a6c292aac4fe9dd3e3323cbd8ef2b2e6430b66ce
minaminoki <sunahukinn1353@me.com>
2017-10-06 22:27:23 -0700

    add boost test of thread and xmlparser

---
+++ boost/thread/thread.cpp
@@ -0,0 +1,30 @@
+#include <boost/thread.hpp>
+#include <iostream>
+#define MAX 5
+ 
+void
+thread_1 () {
+        for (int i = 0; i < MAX; i++) {
+                std::cout << "hello" << std::endl;
+                boost::this_thread::sleep(boost::posix_time::milliseconds(100));
+        }
+}
+void
+thread_2 () {
+        for (int i = 0; i < MAX; i++) {
+                std::cout << "world" << std::endl;
+                boost::this_thread::sleep(boost::posix_time::milliseconds(100));
+        }
+}
+ 
+int
+main(int argc, char const* argv[])
+{
+        boost::thread th1(thread_1);
+        boost::thread th2(thread_2);
+ 
+        th1.join ();
+        th2.join ();
+ 
+        return 0;
+}
---
+++ boost/xmlparser/xmlparser.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/xml_parser.hpp>
+#include <boost/foreach.hpp>
+#include <boost/lexical_cast.hpp>
+
+int main()
+{
+    boost::property_tree::ptree pt;
+    boost::property_tree::read_xml("data.xml", pt);
+
+    if (boost::optional<std::string> str = pt.get_optional<std::string>("root.str")) {
+        std::cout << str.get() << std::endl;
+    }
+    else {
+        std::cout << "root.str is nothing" << std::endl;
+    }
+
+    BOOST_FOREACH (const boost::property_tree::ptree::value_type& child, pt.get_child("root.values")) {
+        const int value = boost::lexical_cast<int>(child.second.data());
+        std::cout << value << std::endl;
+    }
+}
+
