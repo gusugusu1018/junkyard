360a63263b5b16ece725f2002f3f0b6f3a5d91b8
minaminoki <sunahukinn1353@me.com>
2018-11-06 16:47:33 +0900

    add create panorama program

---
+++ opencv/panorama/panorama/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using OpenCvSharp;
+
+namespace panorama
+{
+    class Program
+    {
+        private static Mat[] SelectStitchingImages(int count)
+        {
+            var rand = new Random();
+            var mats = new List<Mat>();
+            for (int i = 0; i < count; i++)
+            {
+                var filename = "C:\\Users\\sv06\\Documents\\junkyard\\opencv\\panorama\\srck" + i.ToString() + ".JPG";
+                var m = new Mat(filename, ImreadModes.Color);
+                mats.Add(m.Clone());
+            }
+
+            return mats.ToArray();
+        }
+        static void Main(string[] args)
+        {
+            Mat[] images = SelectStitchingImages(4);
+            Mat pano = new Mat();
+            var stitcher = Stitcher.Create(false);
+            Console.Write("Stitching start...");
+            var status = stitcher.Stitch(images, pano);
+            Console.WriteLine(" finish (status:{0})", status);
+            Window.ShowImages(pano);
+            foreach (Mat image in images)
+            {
+                image.Dispose();
+            }
+            pano.ImWrite("dst.jpg");
+        }
+    }
+}
